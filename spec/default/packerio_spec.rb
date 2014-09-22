1a0549bb741faa03448a282c3f87981fdeeed2bf
Mark Kusch <mark.kusch@gmail.com>
2014-09-22 16:39:40 +0200

    Add serverspec tests

--- spec/default/packerio_spec.rb
+++ spec/default/packerio_spec.rb
@@ -2,3 +2,13 @@ require 'spec_helper'
 
 # write up RSpec integration tests here
 #   see: http://serverspec.org/resource_types.html
+describe file('/opt/packerio') do
+  it { should be_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+  it { should be_mode 755 }
+end
+
+describe file('/usr/bin/packer') do
+  it { should be_linked_to '/opt/packerio/0.7.1/packer' }
+end
